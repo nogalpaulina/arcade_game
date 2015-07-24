73e56064f016a4fd4bb80ce601e6a2b7f58b4d1e
Paulina Nogal <nogalpaulina@gmail.com>
2015-07-23 20:18:55 -0400

    Add enemies; set different speed for each enemy

--- js/app.js
+++ js/app.js
@@ -1,11 +1,10 @@
 // Enemies our player must avoid
-var Enemy = function() {
-
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+// Parameter: y, the enemy's y coordinate
+// Patameter: speed, the speed of the enemy
+var Enemy = function(y, speed) {
+    this.x = -101;
+    this.y = y;
+    this.speed = speed;
     this.sprite = 'images/enemy-bug.png';
 }
 
@@ -15,6 +14,10 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x = this.x + (this.speed * dt);
+    if (this.x >= 505) {
+        this.x = -101;
+    }
 }
 
 // Draw the enemy on the screen, required method for game
@@ -54,7 +57,7 @@ Player.prototype.handleInput = function(direction) {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 
-var allEnemies = [];
+var allEnemies = [new Enemy(72, 150), new Enemy(155, 200), new Enemy(238, 100)];
 
 // Place the player object in a variable called player
 
